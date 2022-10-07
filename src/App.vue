7bd47da6de19111242117db17b8ac892eef17157
Pin Phearak <phearakroun@gmail.com>
2022-10-07 21:00:52 +0700

    Vue Tinymce

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tinymce id="d1" v-model="data"></tinymce>
+    <button type="button" @click="clickHandler">Reset</button>
+    <div>{{ data }}</div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import tinymce from './components/HelloWorld.vue'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    tinymce
+  },
+  data(){
+      return {
+          data : '<p>Hello from tinymce</p>'
+      };
+  },
+  methods: {
+      clickHandler(){
+          this.data = 'You reseted tinymce\'s content';
+      }
   }
 }
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,167 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+      <textarea :id="id"></textarea>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    import tinymce from 'tinymce/tinymce';
+    import 'tinymce/themes/modern/theme';
+    import 'tinymce/plugins/advlist';
+    import 'tinymce/plugins/wordcount';
+    import 'tinymce/plugins/autolink';
+    import 'tinymce/plugins/autosave';
+    import 'tinymce/plugins/charmap';
+    import 'tinymce/plugins/codesample';
+    import 'tinymce/plugins/contextmenu';
+    import 'tinymce/plugins/emoticons';
+    import 'tinymce/plugins/fullscreen';
+    import 'tinymce/plugins/hr';
+    import 'tinymce/plugins/imagetools';
+    import 'tinymce/plugins/insertdatetime';
+    import 'tinymce/plugins/link';
+    import 'tinymce/plugins/media';
+    import 'tinymce/plugins/noneditable';
+    import 'tinymce/plugins/paste';
+    import 'tinymce/plugins/print';
+    import 'tinymce/plugins/searchreplace';
+    import 'tinymce/plugins/tabfocus';
+    import 'tinymce/plugins/template';
+    import 'tinymce/plugins/textpattern';
+    import 'tinymce/plugins/visualblocks';
+    import 'tinymce/plugins/anchor';
+    import 'tinymce/plugins/autoresize';
+    import 'tinymce/plugins/bbcode';
+    import 'tinymce/plugins/code';
+    import 'tinymce/plugins/colorpicker';
+    import 'tinymce/plugins/directionality';
+    import 'tinymce/plugins/fullpage';
+    import 'tinymce/plugins/help';
+    import 'tinymce/plugins/image';
+    import 'tinymce/plugins/importcss';
+    import 'tinymce/plugins/legacyoutput';
+    import 'tinymce/plugins/lists';
+    import 'tinymce/plugins/nonbreaking';
+    import 'tinymce/plugins/pagebreak';
+    import 'tinymce/plugins/preview';
+    import 'tinymce/plugins/save';
+    import 'tinymce/plugins/spellchecker';
+    import 'tinymce/plugins/table';
+    import 'tinymce/plugins/textcolor';
+    import 'tinymce/plugins/toc';
+    import 'tinymce/plugins/visualchars';
+    import 'tinymce/skins/lightgray/skin.min.css'
+   
+    export default {
+        name: 'HelloWordl',
+        props: { 
+                id : {
+                    type : String,
+                    required : true
+                },
+                htmlClass : { default : '', type : String},
+                value : { default : '' },
+                plugins : { default : function(){ 
+                                    return [
+                                        'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                                        'searchreplace wordcount visualblocks visualchars code fullscreen',
+                                        'insertdatetime media nonbreaking save table contextmenu directionality',
+                                        'template paste textcolor colorpicker textpattern imagetools toc help emoticons hr codesample'
+                                    ];
+                                } , type: Array
+                            },
+                toolbar1: { default :'formatselect | bold italic  strikethrough  forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat', type: String},
+                toolbar2: { default : '', type: String },
+                other_options: { default : function() { return {}; }, type: Object},
+                readonly: { default: false, type: Boolean }
+        },
+        data(){
+            return {
+                content : '',
+                editor : null,
+                cTinyMce : null,
+                checkerTimeout: null,
+                isTyping : false
+            }; 
+        },
+        mounted(){
+            this.content = this.value;
+            this.init();  
+        },
+        beforeDestroy () {
+            this.editor.destroy();
+        },
+        watch: {
+            value : function (newValue){
+                if(!this.isTyping){
+                    if(this.editor !== null)
+                        this.editor.setContent(newValue);
+                    else
+                        this.content = newValue;
+                }
+            },
+            readonly(value){
+                if(value){
+                    this.editor.setMode('readonly');
+                } else {
+                    this.editor.setMode('design');
+                }
+            }
+        },
+        methods: {
+            init(){
+                let options = {
+                    selector: '#' + this.id,
+                    skin: false,
+                    toolbar1: this.toolbar1,
+                    toolbar2: this.toolbar2,
+                    plugins: this.plugins,
+                    init_instance_callback : this.initEditor
+                };
+                tinymce.init(this.concatAssciativeArrays(options, this.other_options));
+            },
+            initEditor(editor) {
+                this.editor = editor;
+                editor.on('KeyUp', () => {
+                    this.submitNewContent();
+                });
+                editor.on('Change', (e) => {
+                    if(this.editor.getContent() !== this.value){
+                        this.submitNewContent();
+                    }
+                    this.$emit('editorChange', e);
+                });
+                editor.on('init', () => {
+                    editor.setContent(this.content);
+                    this.$emit('input', this.content);
+                });
+                if(this.readonly){
+                    this.editor.setMode('readonly');
+                } else {
+                    this.editor.setMode('design');
+                }
+                this.$emit('editorInit', editor);
+            },
+            concatAssciativeArrays(array1, array2){
+                if(array2.length === 0) return array1;
+                if(array1.length === 0) return array2;
+                let dest = [];
+                for ( let key in array1) dest[key] = array1[key];
+                for ( let key in array2) dest[key] = array2[key];
+                return dest;
+            },
+            submitNewContent(){
+                this.isTyping = true;
+                if(this.checkerTimeout !== null)
+                    clearTimeout(this.checkerTimeout);
+                    this.checkerTimeout = setTimeout(()=>{
+                        this.isTyping = false;
+                    }, 300);
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+                this.$emit('input', this.editor.getContent());
+            }
+        }
+    }
+</script>
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
